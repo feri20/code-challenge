5e3c233782d286a18fe0edb3917801479ba825fc
feri20 <f_naderi66@yahoo.com>
2024-06-27 13:13:43 +0330

    Update sql-challenges.sql

--- src/main/java/com/wolf/hackerrank/sql-challenges.sql
+++ src/main/java/com/wolf/hackerrank/sql-challenges.sql
@@ -1,2 +1,2 @@
 select (select count(city) from station)-(select count(distinct city) from station) as difference;
-select city , length(city) as number from station where length(city) = (select min(length(city)) from station)
+select city , length(city) as number from station where length(city) = (select min(length(city)) from station) order by city ASC limit 1;
