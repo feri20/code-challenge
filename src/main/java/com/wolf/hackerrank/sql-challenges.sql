b4cc2a7edc009220e7badc9eeed6b329ac96715e
feri20 <f_naderi66@yahoo.com>
2024-07-08 16:50:18 +0330

    Add sql

--- src/main/java/com/wolf/hackerrank/sql-challenges.sql
+++ src/main/java/com/wolf/hackerrank/sql-challenges.sql
@@ -3,4 +3,6 @@ select city , length(city) as number from station where length(city) = (select m
 select city from station where city like 'i%' or city like 'u%' or city like 'e%' or city like 'a%' or city like 'o%';
 select distinct city from station where city REGEXP '^[aeiouAEIOU]';/* return cities that start with vowels */
 select city from station where city not REGEXP '^[aeiouAEIOU].*[aeiouAEIOU]$';/* return cities that does not start and end with vowels */
-
+select name from students where marks>75 order by substring(name,-3) ; /*order result based on last three character */
+select name from students where marks>75 order by substring(name,3),id ; /*order result based on first three character
+                                                                           and if there is two words the same sort them based on id */
