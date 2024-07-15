ad21dc3a640ab7c80daffa73425b1783d7f06684
feri20 <f_naderi66@yahoo.com>
2024-07-16 01:24:43 +0330

    Add valid username check challenge

---
+++ src/test/java/com/wolf/hackerrank/ValidUserName.java
@@ -0,0 +1,18 @@
+package com.wolf.hackerrank;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+public class ValidUserName {
+
+    @Test
+    void validityCheck(){
+        String userName = "_julia456123";
+        if(userName.length()<8|userName.length()>30){
+            System.out.println("Invalid");
+        }else if (!Pattern.matches("^[a)-zA-Z][a-zA-Z0-9_]*$",userName)){
+            System.out.println("Valid");
+        }
+    }
+}
